e28213ab5673bed2802cfc61c1831514d782f906
David Steinmann <david.steinmann@gymspit.cz>
2024-11-12 09:48:27 +0100

    added Lecture10, basic layout

---
+++ Lecture10/Lecture10/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Lecture10
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            helloLabel = new Label();
+            button1 = new Button();
+            richTextBox1 = new RichTextBox();
+            button2 = new Button();
+            panel1 = new Panel();
+            tableLayoutPanel1 = new TableLayoutPanel();
+            splitContainer1 = new SplitContainer();
+            richTextBox3 = new RichTextBox();
+            richTextBox2 = new RichTextBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // helloLabel
+            // 
+            helloLabel.Dock = DockStyle.Top;
+            helloLabel.Font = new Font("Segoe UI", 20F, FontStyle.Regular, GraphicsUnit.Point);
+            helloLabel.Location = new Point(0, 0);
+            helloLabel.Name = "helloLabel";
+            helloLabel.Size = new Size(800, 58);
+            helloLabel.TabIndex = 0;
+            helloLabel.Text = "Testdawdawd";
+            helloLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(473, 147);
+            button1.Name = "button1";
+            button1.Size = new Size(96, 32);
+            button1.TabIndex = 1;
+            button1.Text = "click me!";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(368, 225);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(217, 109);
+            richTextBox1.TabIndex = 2;
+            richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(510, 76);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(tableLayoutPanel2);
+            panel1.Controls.Add(tableLayoutPanel1);
+            panel1.Controls.Add(splitContainer1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(helloLabel);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(richTextBox1);
+            panel1.Dock = DockStyle.Fill;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(800, 450);
+            panel1.TabIndex = 4;
+            // 
+            // tableLayoutPanel1
+            // 
+            tableLayoutPanel1.ColumnCount = 2;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Location = new Point(162, 309);
+            tableLayoutPanel1.Name = "tableLayoutPanel1";
+            tableLayoutPanel1.RowCount = 2;
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Size = new Size(200, 100);
+            tableLayoutPanel1.TabIndex = 5;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Location = new Point(45, 96);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(richTextBox3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(richTextBox2);
+            splitContainer1.Size = new Size(268, 207);
+            splitContainer1.SplitterDistance = 95;
+            splitContainer1.TabIndex = 4;
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.Dock = DockStyle.Fill;
+            richTextBox3.Location = new Point(0, 0);
+            richTextBox3.Name = "richTextBox3";
+            richTextBox3.Size = new Size(95, 207);
+            richTextBox3.TabIndex = 0;
+            richTextBox3.Text = "";
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.Dock = DockStyle.Fill;
+            richTextBox2.Location = new Point(0, 0);
+            richTextBox2.Name = "richTextBox2";
+            richTextBox2.Size = new Size(169, 207);
+            richTextBox2.TabIndex = 0;
+            richTextBox2.Text = "";
+            // 
+            // tableLayoutPanel2
+            // 
+            tableLayoutPanel2.ColumnCount = 2;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Location = new Point(558, 320);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 2;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(200, 100);
+            tableLayoutPanel2.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Label helloLabel;
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Button button2;
+        private Panel panel1;
+        private SplitContainer splitContainer1;
+        private RichTextBox richTextBox3;
+        private RichTextBox richTextBox2;
+        private TableLayoutPanel tableLayoutPanel1;
+        private TableLayoutPanel tableLayoutPanel2;
+    }
+}
---
+++ Lecture10/Lecture10/Form1.cs
@@ -0,0 +1,15 @@
+namespace Lecture10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            helloLabel.Text = "Hello World!";
+        }
+    }
+}
