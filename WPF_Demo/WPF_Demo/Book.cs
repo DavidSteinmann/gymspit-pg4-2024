684ea7eaba80b8f23d58538e6482e2f07f617ec6
David Steinmann <david.steinmann@gymspit.cz>
2024-12-03 09:49:53 +0100

    added WPF_Demo

---
+++ WPF_Demo/WPF_Demo/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Demo;
+
+internal class Book : INotifyPropertyChanged
+{
+    private string title;
+
+    public string Title
+    {
+        get => title;
+        set {
+            title = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Title)));
+        }
+    }
+
+    public string Description { get; set; }
+    public string Author { get; set; }
+
+    public Book(string title, string description, string author)
+    {
+        Title = title;
+        Description = description;
+        Author = author;
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public override string? ToString()
+    {
+        return $"{Title} {Description} {Author}";
+    }
+}
---
+++ WPF_Demo/WPF_Demo/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Printing;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<string> Strings = new ObservableCollection<string>();
+        //List<string> Strings = new List<string>();
+
+        ObservableCollection<Book> books = new ObservableCollection<Book>();
+        Book MyBook { get; set; } = new Book("CC", "DD", "EE");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            //TestList.DataContext = Strings;
+            TestList.DataContext = books;
+            TitleLabel.DataContext = MyBook;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //TitleLabel.Content = "Hello world!";
+            Strings.Add("Hello world");
+            books.Add(new Book("A", "B", "C"));
+            MyBook.Title = "AAWDAWD";
+        }
+    }
+}
